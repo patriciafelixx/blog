5b45bdd6581e96b2142a436d07420aba3bfaa94f
unknown <patriciafelixx@outlook.com>
2020-10-10 12:29:29 -0300

    Timezone Config

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 const dbConnection = new Sequelize('blog', 'root', 'root', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 })
 
 module.exports = dbConnection;
