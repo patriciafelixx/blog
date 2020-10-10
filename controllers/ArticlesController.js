94d8a47de1598f732a79fe486ad3521cd0cbb143
unknown <patriciafelixx@outlook.com>
2020-10-10 12:25:32 -0300

    Articles CRUD

--- controllers/ArticlesController.js
+++ controllers/ArticlesController.js
@@ -1,6 +1,68 @@
+const slugify = require('slugify');
+
+const Article = require('../models/Article');
+const Category = require('../models/Category');
+
 const ArticlesController = {
-    index: (req, res) => {
-        res.send('Rota de artigos');
+    index: async (req, res) => {
+        const articles = await Article.findAll({
+            include: [{
+                model: Category
+            }]
+        });
+        res.render('articles/index', { articles });
+    },
+    new: async (req, res) => {
+        const categories = await Category.findAll();
+        res.render('articles/new', { categories });
+    },
+    save: async (req, res) => {
+        const { title, body, category } = req.body;
+        if (title && body && category) {
+            await Article.create({
+                title,
+                body,
+                slug: slugify(title.toLowerCase()),
+                categoryId: category
+            })
+            res.redirect('/admin/articles')
+        } else {
+            return
+        }
+    },
+    show: async (req, res) => {
+        const { id } = req.params;
+        const categories = await Category.findAll();
+        const article = await Article.findOne({ 
+            where: { id },
+            include: [{
+                model: Category
+            }]
+        });
+        res.render('articles/update', { article, categories });
+    },
+    update: async (req, res) => {
+        const { id } = req.params;
+        const { title, body, category } = req.body;
+        await Article.update({
+            title,
+            body,
+            slug: slugify(title.toLowerCase()),
+            categoryId: category
+        }, {
+            where: { id }
+        })
+
+        res.redirect('/admin/articles');
+
+    },
+    delete: async (req, res) => {
+        const { id } = req.params;
+        await Article.destroy({
+            where: { id }
+        })
+
+        res.redirect('/admin/articles');
     }
 }
 
--- routes.js
+++ routes.js
@@ -14,6 +14,11 @@ routes.get('/categories/update/:id', CategoriesController.show);
 routes.post('/categories/update/:id', CategoriesController.update);
 routes.post('/categories/delete/:id', CategoriesController.delete);
 
-routes.get('/articles', ArticlesController.index);
+routes.get('/admin/articles', ArticlesController.index);
+routes.get('/admin/articles/new', ArticlesController.new);
+routes.post('/admin/articles/save', ArticlesController.save);
+routes.get('/articles/update/:id', ArticlesController.show);
+routes.post('/articles/update/:id', ArticlesController.update);
+routes.post('/articles/delete/:id', ArticlesController.delete);
 
 module.exports = routes;
