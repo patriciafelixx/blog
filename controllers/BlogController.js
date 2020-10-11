2418e4ee940047fcc9673d23958e58f8ba1dd52d
unknown <patriciafelixx@outlook.com>
2020-10-11 12:58:44 -0300

    Blog homepage with category filters

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -1,6 +1,48 @@
+const Article = require('../models/Article');
+const Category = require('../models/Category');
+
 const BlogController = {
-    index: (req, res) => {
-        res.render('index');
+    index: async (req, res) => {
+        const articles = await Article.findAll({
+            include: [{
+                model: Category
+            }],
+            order: [
+                ['id', 'DESC']
+            ]
+        });
+        const categories = await Category.findAll();
+
+        res.render('index', { articles, categories });
+    },
+    indexFilter: async (req,res) => {
+        const { slug } = req.params;
+        
+        const category = await Category.findOne({
+            where: { slug },
+            include: [{
+                model: Article
+            }]
+        })
+        
+        const categories = await Category.findAll();
+        
+        res.render('indexFilter', { articles: category.articles, category, categories });
+    },
+    slug: async (req, res) => {
+        const { slug } = req.params;
+        const article = await Article.findOne({
+            where: { slug },
+            include: [{
+                model: Category
+            }]
+        })
+
+        if (article) {
+            res.render('articles/article', { article });
+        } else {
+            res.redirect('/');
+        }
     }
 }
 
--- routes.js
+++ routes.js
@@ -6,19 +6,21 @@ const CategoriesController = require('./controllers/CategoriesController');
 const ArticlesController = require('./controllers/ArticlesController');
 
 routes.get('/', BlogController.index);
+routes.get('/category/:slug', BlogController.indexFilter);
+routes.get('/:slug', BlogController.slug);
 
 routes.get('/admin/categories/new', CategoriesController.new);
 routes.get('/admin/categories', CategoriesController.index);
-routes.post('/categories/save', CategoriesController.save);
-routes.get('/categories/update/:id', CategoriesController.show);
-routes.post('/categories/update/:id', CategoriesController.update);
-routes.post('/categories/delete/:id', CategoriesController.delete);
+routes.post('/admin/categories/save', CategoriesController.save);
+routes.get('/admin/categories/update/:id', CategoriesController.show);
+routes.post('/admin/categories/update/:id', CategoriesController.update);
+routes.post('/admin/categories/delete/:id', CategoriesController.delete);
 
 routes.get('/admin/articles', ArticlesController.index);
 routes.get('/admin/articles/new', ArticlesController.new);
 routes.post('/admin/articles/save', ArticlesController.save);
-routes.get('/articles/update/:id', ArticlesController.show);
-routes.post('/articles/update/:id', ArticlesController.update);
-routes.post('/articles/delete/:id', ArticlesController.delete);
+routes.get('/admin/articles/update/:id', ArticlesController.show);
+routes.post('/admin/articles/update/:id', ArticlesController.update);
+routes.post('/admin/articles/delete/:id', ArticlesController.delete);
 
 module.exports = routes;
